b82a976519e1d1eb6feb38180f92117abfc2bcb9
Nikhil Yekhe <yendenikhil@gmail.com>
2022-01-02 23:59:44 +0100

    day 19 done and with this all is finished! Merry (belated) xmas!!

--- 19.ts
+++ 19.ts
@@ -1,6 +1,8 @@
 const p = console.log;
-const raw = await Deno.readTextFile("19.1.txt");
+const raw = await Deno.readTextFile("19.txt");
 type Pos = [number, number, number];
+const cp = (p1: Pos, p2: Pos): boolean =>
+  p1[0] === p2[0] && p1[1] === p2[1] && p1[2] === p2[2];
 
 class Scanner {
   id: number;
@@ -22,6 +24,47 @@ class Scanner {
     this.seq();
   }
   seq(): void {
+    this.bb.push(...zip(...this.b.map((pos) => this.tr(pos))));
+    /*
+    const tmp: Pos[][] = this.b.map((pos) => this.tr(pos))
+    for(let i = 0; i < 24; i++) {
+      const t: Pos[] = []
+      t.push(...tmp.map(e => e[i]))
+      this.bb.push(t)
+    }
+    */
+  }
+  tr(pos: Pos): Pos[] {
+    const [x, y, z] = pos;
+    return [
+      [x, y, z],
+      [z, y, -x],
+      [-x, y, -z],
+      [-z, y, x],
+      [-y, x, z],
+      [z, x, y],
+      [y, x, -z],
+      [-z, x, -y],
+      [y, -x, z],
+      [z, -x, -y],
+      [-y, -x, -z],
+      [-z, -x, y],
+      [x, -z, y],
+      [y, -z, -x],
+      [-x, -z, -y],
+      [-y, -z, x],
+      [x, -y, -z],
+      [-z, -y, -x],
+      [-x, -y, z],
+      [z, -y, x],
+      [x, z, -y],
+      [-y, z, -x],
+      [-x, z, y],
+      [y, z, x],
+    ];
+  }
+  /*
+   seq(): void {
     const roll = ([x, y, z]: Pos): Pos => [x, z, -1 * y];
     const turn = ([x, y, z]: Pos): Pos => [-1 * y, x, z];
     let v = this.b.slice();
@@ -39,35 +82,77 @@ class Scanner {
       });
     });
   }
+  */
 }
 
-const comparePosition = (p1: Pos[], p2: Pos[]): number => {
-  // p({p1, p2})
-  let max = 0;
-  for (let i = 0; i < p1.length; i++) {
-    const [dx1, dy1, dz1] = p1[i];
-    for (let j = 0; j < p2.length; j++) {
-      const [dx2, dy2, dz2] = p2[j];
-      const shifted1: string[] = p1.map((
-        [ddx, ddy, ddz],
-      ) => [ddx - dx1, ddy - dy1, ddz - dz1]).map((e) => JSON.stringify(e));
-      const shifted2: string[] = p2.map((
-        [ddx, ddy, ddz],
-      ) => [ddx - dx2, ddy - dy2, ddz - dz2]).map((e) => JSON.stringify(e));
-      const count = shifted1.filter((e) => shifted2.includes(e)).length;
-      max = max > count ? max : count;
-      // p({ max, count });
-      // p({shifted1, shifted2})
-    }
+const zip = <T>(...arr: T[][]): T[][] => {
+  const ans: T[][] = [];
+  const len = Math.min(...arr.map((e) => e.length));
+  for (let i = 0; i < len; i++) {
+    ans.push(arr.map((e) => e[i]));
   }
-
-  return max;
+  return ans;
 };
-
 const scanners: Scanner[] = raw.trim().split("\n\n").map((block) =>
   block.split("\n")
 ).map((lines) => new Scanner(lines));
 scanners[0].s = [0, 0, 0];
-// p(scanners)
-scanners[1].bb.map(bb => comparePosition(scanners[0].b, bb))
-          .forEach((e, i) => p({i, e}))
+
+const md = (p1: Pos | undefined, p2: Pos | undefined): number => {
+  if (p1 === undefined || p2 === undefined) return -1;
+  return zip(p1, p2).map(([a, b]) => Math.abs(a - b)).reduce((a, b) => a + b);
+};
+
+console.time("a");
+while (scanners.filter((s) => s.s === undefined).length > 0) {
+  for (const s2 of scanners.filter((s) => s.s === undefined)) {
+    for (const ps2 of s2.bb) {
+      const dist: Map<string, number> = new Map();
+      const intersection: Map<string, Array<[Pos, Pos]>> = new Map();
+      // p({s2: s2.id, s1s: scanners.filter(s => s.s !== undefined).map(s => s.id)})
+      for (
+        const p1 of scanners.filter((s) => s.s !== undefined).map((s) => s.b)
+          .flat()
+      ) {
+        for (const p2 of ps2) {
+          const diff = zip(p1, p2).map(([a, b]) => a - b);
+          const key = JSON.stringify(diff);
+          dist.set(key, (dist.get(key) ?? 0) + 1);
+          const pairs = intersection.get(key) ?? [];
+          pairs.push([p1, p2]);
+          intersection.set(key, pairs);
+        }
+      }
+      const count = Math.max(...dist.values());
+      if (count >= 12) {
+        let key = "";
+        intersection.forEach((v, k) => {
+          if (v.length >= 12) {
+            key = k;
+            // p({k, l: v.length, s2: s2.id})
+            // p(v)
+          }
+        });
+        const [xo, yo, zo] = JSON.parse(key);
+        s2.s = [xo, yo, zo];
+        s2.b = ps2.map(([x, y, z]) => [x + xo, y + yo, z + zo]);
+        break;
+      }
+    }
+  }
+}
+const p1a = scanners.map((s) => s.b).flat().reduce((s, x) => {
+  s.add(JSON.stringify(x));
+  return s;
+}, new Set());
+const p2 = Math.max(
+  ...scanners.map((s1) =>
+    Math.max(
+      ...scanners.filter((s2) => s1.id !== s2.id).map((s2) =>
+        md(s1.s, s2.s)
+      ),
+    )
+  ),
+);
+p({ p1: p1a.size, p2 });
+console.timeEnd("a");
